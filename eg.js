eb8e9bf3bf8880ffd77375594e208f865e3f2284
Priya-191103 <priyamoni1912@gmail.com>
2024-10-18 12:28:11 +0530

    proect01

---
+++ eg.js
@@ -0,0 +1,15 @@
+let grade = 'B';
+
+switch (grade) {
+  case 'A':
+    console.log("Excellent");
+    break;
+  case 'B':
+    console.log("Good");
+    break;
+  case 'C':
+    console.log("Average");
+    break;
+  default:
+    console.log("Invalid grade");
+}
